97fc4bb6d965d86e4f38e4f4de80d70981eaa7c1
Quasimodo <quasimodo1660@gmail.com>
2018-03-05 13:28:08 -0800

    Length of Last Word

--- Basic algorithm.js
+++ Basic algorithm.js
@@ -7,3 +7,21 @@ var isPalindrome = function(x) {
     else
         return false;
 };
+//-------------------------------------------------------------------------------------------
+//Length of Last Word
+// Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string
+// If the last word does not exist, return 0
+// Note: A word is defined as a character sequence consists of non-space characters only.
+// Example:
+// Input: "Hello World"
+// Output: 5
+var lengthOfLastWord = function(s) {
+    var arr = s.split(' ').reverse();
+    for (var x=0;x<arr.length;x++){
+        if(arr[x]!='')
+            return arr[x].length;
+    }
+    return 0;
+};
+
+console.log(lengthOfLastWord(' a'))
