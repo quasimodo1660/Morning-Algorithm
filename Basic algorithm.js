45a38f6ebc66ad654cbc64dad2e78e60f60a7e46
Quasimodo <quasimodo1660@gmail.com>
2018-03-05 16:09:17 -0800

    Happy Number

--- Basic algorithm.js
+++ Basic algorithm.js
@@ -25,3 +25,28 @@ var lengthOfLastWord = function(s) {
 };
 
 console.log(lengthOfLastWord(' a'))
+
+// Happy Number
+// Write an algorithm to determine if a number is "happy".
+
+// A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+// Example: 19 is a happy number
+
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+var isHappy = function(n) {
+    if (n>6){
+        var sum=String(n).split('').reduce((x,y)=> x+=Math.pow(parseInt(y),2),0);
+        if (sum==1)
+            return true;
+        else
+            return isHappy(sum);
+    }
+    else
+        return n==1
+};
+
+console.log(isHappy(2))
