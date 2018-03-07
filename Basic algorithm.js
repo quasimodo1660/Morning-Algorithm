99dccf7fe489be1380eeca113db425e006403cf4
Quasimodo <quasimodo1660@gmail.com>
2018-03-07 14:26:55 -0800

    Self Dividing Numbers

--- Basic algorithm.js
+++ Basic algorithm.js
@@ -165,4 +165,35 @@ var isPalindrome = function(s) {
         return s==s.split('').reverse().join('')
     }
 };
-console.log(isPalindrome("race a car"))
+//-------------------------------------------------------------------------------------------
+//Self Dividing Numbers
+// A self-dividing number is a number that is divisible by every digit it contains.
+// For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+// Also, a self-dividing number is not allowed to contain the digit zero.
+// Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
+// Example 1:
+// Input: 
+// left = 1, right = 22
+// Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+// Note:
+// The boundaries of each input argument are 1 <= left <= right <= 10000.
+var selfDividingNumbers = function(left, right) {
+  var arr = [];
+  var insert;
+    for(var x=left;x<=right;x++){
+        insert=true;
+        if(x.toString().includes('0'))  
+            insert=false;
+        else{
+            for(var i=0;i<x.toString().split('').length;i++){
+                if(x%Number(x.toString().split('')[i])!=0){
+                    insert=false;
+                    break;
+                }       
+            }
+        }
+        if(insert)
+            arr.push(x)       
+  }  
+  return arr
+};
