740b93edc9831f309ab0c44fb8f22a9d9939ee1d
Quasimodo <quasimodo1660@gmail.com>
2018-03-19 14:14:29 -0700

    Product of array except self

--- Basic algorithm.js
+++ Basic algorithm.js
@@ -68,7 +68,6 @@ var isHappy = function(n) {
 // No such pair of words.
 var maxProduct = function(words) {
     var arr=[];
-    var arr2=[];
     var flag;  
     var result=0;
     for (var x=0;x<words.length-1;x++){
@@ -389,3 +388,24 @@ var isPowerOfThree = function(n) {
 };
 
 console.log(isPowerOfThree(45))
+
+//238. Product of Array Except Self
+// Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+// Solve it without division and in O(n).
+// For example, given [1,2,3,4], return [24,12,8,6].
+// Follow up:
+// Could you solve it with constant space complexity? (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
+var productExceptSelf = function(nums) {
+    var arr=[];
+    var product;
+    for(var i =0;i<nums.length;i++){
+        product=1;
+        for (var j=0;j<nums.length;j++){
+            if(i!=j)
+                product*=nums[j]
+        }
+        arr.push(product)
+    }
+    return arr;
+};
+console.log(productExceptSelf([1,2,3,4]))
